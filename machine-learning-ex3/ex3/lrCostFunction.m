0daaa116835e55a3f9decd221d504e5832b28c3d
Marcos Roriz <marcosrorizinf@gmail.com>
2018-01-21 19:34:25 -0200

    Implemented the regularized logistic regression.

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,6 +36,31 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+% ====== COST ==================================================
+HARG = X * theta;      % HIPOTHESIS ARGUMENT
+HIPO = sigmoid(HARG);  % LOGISTIC HIPOTHESIS
+
+POSCLASSCOST = -1 .* y .* log(HIPO);
+NEGCLASSCOST = (1 .- y) .* log(1 .- HIPO);
+
+REGTHETA    = theta;
+REGTHETA(1) = 0;
+REGSQUARE   = REGTHETA .^ 2;
+REGULARIZER = (lambda / (2*m)) .* sum(REGSQUARE);
+
+J = ((1 / m) * (sum(POSCLASSCOST) - sum(NEGCLASSCOST))) + REGULARIZER;
+% =============================================================
+
+
+% ====== GRAD ==================================================
+HE = HIPO - y;                 % HIPOTHESIS ERROR
+RG = (lambda / m) .* REGTHETA; % REGULARIZER DERIVATIVE VECTOR
+
+% WE WILL TRANSPOSE X SO WE CAN MULTIPLY ALL FEATURES AT THE SAME TIME AND 
+% SUM UP THE ERROR OF EACH THETA J AND THE REGULARIZER DERIVATIVE.
+grad = ((1 / m) .* (X.' * HE)) + RG;
+
+% =============================================================
 
 
 
